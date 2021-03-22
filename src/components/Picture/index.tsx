45139616b1129362e9976e4b20f7fed4e99a768f
Diego Almirón <diegoalmesp@gmail.com>
2021-03-22 02:08:41 -0300

    config on env file

--- src/components/Picture/index.tsx
+++ src/components/Picture/index.tsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from "react";
 
 import { createClient } from "pexels";
 
-const client = createClient(
-  "563492ad6f91700001000001dc9aa6b52d8246b1a454f08bde71b654"
-);
+/**
+ * Diego:
+ * in a bigger app I would move the process env constants to a config file
+ */
+const client = createClient(process.env.REACT_APP_PEXELS_API as string);
 
 const Picture: React.FC<{ search: string }> = ({ search }) => {
   const [image, setImage] = useState<any>();
